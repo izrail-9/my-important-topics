4e10cf00fe373a63231759044bb6e5e3331876c9
Izrail <izrailaryan13@gmail.com>
2019-06-25 21:50:04 +0530

    Create cool.cpp

---
+++ cool.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  /*  auto n=1LL;cin>>n;
+    cout<<floor(log10(n)) + 1;
+    */
+    /*
+    long long int n=16544;
+   double K = log10(n);
+    K = K - floor(K);
+   int X = pow(10, K);
+   cout<<X;
+   */
+
+}
