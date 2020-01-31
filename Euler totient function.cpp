6c88ff40fae142ca9434fe4a3be350901e35af39
Izrail <izrailaryan13@gmail.com>
2020-01-31 17:49:55 +0530

    Update Euler totient function.cpp

--- Euler totient function.cpp
+++ Euler totient function.cpp
@@ -1,4 +1,4 @@
-#include <stdio.h> 
+/*#include <stdio.h> 
 
 int phi(int n) 
 { 
@@ -27,7 +27,34 @@ int phi(int n)
 
 	return (int)result; 
 } 
+*/
+// for avoiding float point numbers
+#include <stdio.h> 
+#define ll long long int
+ll phi(ll n) 
+{ 
+	ll result = n; // Initialize result as n 
 
+	// Consider all prime factors of n and subtract their 
+	// multiples from result 
+	for (ll p = 2; p * p <= n; ++p) { 
+		
+		// Check if p is a prime factor. 
+		if (n % p == 0) { 
+			
+			// If yes, then update n and result 
+			while (n % p == 0) 
+				n /= p; 
+			result -= result / p; 
+		} 
+	} 
+
+	// If n has a prime factor greater than sqrt(n) 
+	// (There can be at-most one such prime factor) 
+	if (n > 1) 
+		result -= result / n; 
+	return result; 
+} 
 // Driver program to test above function 
 int main() 
 { 
